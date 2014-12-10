ae7f1fb98c642ea91cee4ccad4d386583b44470f
Rob Eisenberg <rob@bluespire.com>
2014-12-10 00:01:48 -0500

    chore(*): set up the project structure and build
    This commit adds the initial source code, sets up the project
    structure, creates the gulp build file and configures karma.

---
+++ dist/commonjs/index.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var normalize = require('aurelia-path').normalize;
+
+
+var hasTemplateElement = ("content" in document.createElement("template"));
+
+var Loader = (function () {
+  var Loader = function Loader() {};
+
+  Loader.createDefaultLoader = function () {
+    throw new Error("No default loader module imported.");
+  };
+
+  Loader.prototype.loadModule = function (id) {
+    throw new Error("Loaders must implement loadModule(id).");
+  };
+
+  Loader.prototype.loadAllModules = function (ids) {
+    throw new Error("Loader must implement loadAllModules(ids).");
+  };
+
+  Loader.prototype.loadTemplate = function (url) {
+    var _this = this;
+    return new Promise(function (resolve, reject) {
+      url = normalize("./" + url, _this.getBaseUrl());
+
+      _import(url, function (doc) {
+        if (!hasTemplateElement) {
+          HTMLTemplateElement.bootstrap(doc);
+        }
+
+        var template = doc.querySelector("template");
+        resolve(template);
+      });
+    });
+  };
+
+  return Loader;
+})();
+
+exports.Loader = Loader;
+
+
+function importElements(frag, link, callback) {
+  document.head.appendChild(frag);
+
+  if (window.Polymer && Polymer.whenReady) {
+    Polymer.whenReady(function () {
+      return callback(link["import"]);
+    });
+  } else {
+    link.addEventListener("load", function () {
+      return callback(link["import"]);
+    });
+  }
+}
+
+function _import(url, callback) {
+  var frag = document.createDocumentFragment();
+  var link = document.createElement("link");
+
+  link.rel = "import";
+  link.href = url;
+  frag.appendChild(link);
+
+  importElements(frag, link, callback);
+}
---
+++ dist/es6/index.js
@@ -0,0 +1,53 @@
+import {normalize} from 'aurelia-path';
+
+var hasTemplateElement = ('content' in document.createElement('template'));
+
+export class Loader {
+  static createDefaultLoader(){
+    throw new Error('No default loader module imported.');
+  }
+
+  loadModule(id){
+    throw new Error('Loaders must implement loadModule(id).');
+  }
+
+  loadAllModules(ids){
+    throw new Error('Loader must implement loadAllModules(ids).');
+  }
+
+  loadTemplate(url){
+    return new Promise((resolve, reject) => {
+      url = normalize('./' + url, this.getBaseUrl());
+
+      _import(url, doc => {
+        if(!hasTemplateElement){
+          HTMLTemplateElement.bootstrap(doc);
+        }
+
+        var template = doc.querySelector('template');
+        resolve(template);
+      });
+    });
+  }
+}
+
+function importElements(frag, link, callback) {
+  document.head.appendChild(frag);
+
+  if(window.Polymer && Polymer.whenReady){
+    Polymer.whenReady(() => callback(link.import));
+  }else{
+    link.addEventListener('load', () => callback(link.import));
+  }
+}
+
+function _import(url, callback) {
+  var frag = document.createDocumentFragment();
+  var link = document.createElement('link');
+
+  link.rel = 'import';
+  link.href = url;
+  frag.appendChild(link);
+
+  importElements(frag, link, callback);
+}
