5b8302120909bbc6fbc0941ec4256299204c6e80
Rob Eisenberg <rob@bluespire.com>
2015-01-06 11:59:58 -0500

    feat(build): update compile, switch to register modules, switch to core-js

--- config.js
+++ config.js
@@ -1,15 +1,23 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "aurelia-loader/*": "dist/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
 System.config({
   "map": {
-    "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.1",
-    "es6-shim": "github:paulmillr/es6-shim@0.21.1",
-    "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
+    "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
+    "core-js": "npm:core-js@0.4.1",
+    "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2",
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "npm:core-js@0.4.1": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
+    }
   }
 });
 
