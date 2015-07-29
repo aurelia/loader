660c07c13c993cec6530eb094dcc50bf640d1eb2
Rob Eisenberg <rob@bluespire.com>
2015-07-29 17:20:26 -0400

    chore(build): improve output file name

--- config.js
+++ config.js
@@ -17,13 +17,16 @@ System.config({
 System.config({
   "map": {
     "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
-    "aurelia-metadata": "github:aurelia/metadata@0.7.0",
-    "aurelia-path": "github:aurelia/path@0.8.0",
+    "aurelia-metadata": "github:aurelia/metadata@0.7.1",
+    "aurelia-path": "github:aurelia/path@0.8.1",
     "babel": "npm:babel-core@5.1.13",
     "babel-runtime": "npm:babel-runtime@5.1.13",
     "core-js": "npm:core-js@0.9.18",
     "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.1",
     "github:aurelia/metadata@0.7.0": {
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/metadata@0.7.1": {
       "core-js": "npm:core-js@0.9.18"
     },
     "github:jspm/nodelibs-process@0.1.1": {
