14cf298ab9f40159280719b8a38ae1b381f43903
Romke van der Meulen <romke.vd.meulen@gmail.com>
2019-02-18 11:35:22 +0100

    style(template-registry-entry): fix indentation

--- test/template-registry-entry.spec.js
+++ test/template-registry-entry.spec.js
@@ -14,11 +14,11 @@ describe('the template registry entry', () => {
       const entry = new TemplateRegistryEntry('my-module/my-main-component.html');
       entry.template = template;
       expect(entry.dependencies.length).toBe(1);
-	  expect(entry.dependencies[0].src).toBe('my-module/another-component');
+      expect(entry.dependencies[0].src).toBe('my-module/another-component');
 
-	  entry.addDependency('my-module/my-sub-component');
+      entry.addDependency('my-module/my-sub-component');
       expect(entry.dependencies.length).toBe(1);
-	  entry.addDependency('my-module/yet-another-component');
+      entry.addDependency('my-module/yet-another-component');
       expect(entry.dependencies.length).toBe(2);
     });
   });
